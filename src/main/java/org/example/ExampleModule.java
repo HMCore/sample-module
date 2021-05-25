53d988ef1ffb8f1c83edd84e962c9c8ff8931843
Waterdev <30842467+UnrealValentin@users.noreply.github.com>
2021-05-25 14:21:00 +0200

    Update ExampleModule.java

--- src/main/java/org/example/ExampleModule.java
+++ src/main/java/org/example/ExampleModule.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.hmcore.modules.Module;
 
+import static org.hmcore.HMCore.logger;
+
 public class ExampleModule extends Module {
     @Override
     public String getName() {
@@ -13,30 +15,30 @@ public class ExampleModule extends Module {
     @Override
     protected boolean initialize() {
 
-        System.out.print(PREFIX + "Loading darker netzz...");
-        System.out.println(Math.random());
+        logger.info(PREFIX + "Loading darker netzz...");
+        logger.info(Math.random());
 
         return true;
     }
 
     @Override
     protected boolean hook() {
-        System.out.print(PREFIX + "Loading much darker netzz...");
-        System.out.println(Math.floor(Math.random()));
+        logger.info(PREFIX + "Loading much darker netzz...");
+        logger.info(Math.floor(Math.random()));
         return true;
     }
 
     @Override
     protected boolean disable() {
-        System.out.print(PREFIX + "Unloading much darker netzz...");
-        System.out.println(Math.abs(Math.random()));
+        logger.info(PREFIX + "Unloading much darker netzz...");
+        logger.info(Math.abs(Math.random()));
         return true;
     }
 
     @Override
     protected boolean unload() {
-        System.out.print(PREFIX + "Unloading darker netzz...");
-        System.out.println(Math.cos(Math.random()));
+        logger.info(PREFIX + "Unloading darker netzz...");
+        logger.info(Math.cos(Math.random()));
         return true;
     }
 }
